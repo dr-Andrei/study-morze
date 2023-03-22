2d84ba169836fc00b9b91768cea3589f054b0a7b
dr-Andrei <bitchandrei@yandex.ru>
2023-03-22 11:46:12 +0300

    upd

--- src/LearnLetters/index.js
+++ src/LearnLetters/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css';
 import morse from 'morse-decoder';
 const fortune = require('fortune-cookie-ru');
 
@@ -8,12 +9,45 @@ class learnLetters extends React.Component {
         super(props);
 
     }
-
+    playLetters = (e) => {
+        const audio = morse.audio(e.target.attributes[1].textContent, {
+            unit: 0.14, // продолжительность звука
+            fwUnit: 0.3, // время между буквами
+            oscillator: {
+                type: 'sine',
+                frequency: 500
+            }
+        });
+        audio.play(); // play audio
+    }
     render() {
         return (
-            <div>
-                123
-            </div>
+            <article className='article'>
+                <table className='letters-table'>
+                    <thead>
+                        <tr>
+                            <td>Буква</td>
+                            <td>Код</td>
+                            <td>Расшифровка</td>
+                            <td>Воспроизвести</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Й</td>
+                            <td>.---</td>
+                            <td>й-краа-ткооо-ееее</td>
+                            <td><button type="button" data-letter="й" onClick={this.playLetters}>Play</button></td>
+                        </tr>
+                        <tr>
+                            <td>Ы</td>
+                            <td>-.--</td>
+                            <td>ыыы-не-нааа-дооо</td>
+                            <td><button type="button" data-letter="ы" onClick={this.playLetters}>Play</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </article>
         );
     }
 }
