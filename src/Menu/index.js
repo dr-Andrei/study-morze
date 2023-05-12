d389553f1994971e07962c5f8ad3a834c04b3b07
dr-Andrei <bitchandrei@yandex.ru>
2023-05-12 09:04:47 +0300

    update settings menu

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './style.css';
 import img from './images/menu.png';
 
+/**
+ * Всплывающее меню настройки воспроизведения кода
+ */
 
 class Menu extends React.Component {
     constructor(props) {
@@ -13,7 +16,7 @@ class Menu extends React.Component {
     }
 
     showMenu = () => {
-        document.querySelector('.settings-menu_pole').classList.toggle('hide');
+        document.querySelector('.settings-menu_pole').classList.toggle('hs');
     }
 
     render() {
@@ -22,16 +25,22 @@ class Menu extends React.Component {
                 <div className='settings-menu_icon'>
                     <img src={img} alt="" onClick={this.showMenu} />
                 </div>
-                <div className='settings-menu_pole hide'>
+                <div className='settings-menu_pole hs'>
                     <h3>Настройки</h3>
                     <ul className='settings-menu_pole__list'>
                         <li className='settings-menu_pole__list-item'>
-                            <p>Продолжительность звука: <b>{this.state.countLetters}</b></p>
-                            <input className="footer-edit__count-letters" type="range" name="" max="100" min="0" onChange={this.f_countLetters} value={this.state.countLetters} />
+                            <h5 className='settings-menu_pole__p'>Продолжительность звука: <b>{this.state.countLetters}</b></h5>
+                            <div className='settings-menu_pole__out'>
+                                <input className="footer-edit__count-letters" type="range" name="" max="100" min="0" onChange={this.f_countLetters} value={this.state.countLetters} />
+                                <p className='settings-menu_pole__out_n1'>24</p>
+                            </div>
                         </li>
                         <li className='settings-menu_pole__list-item'>
-                            <p>Время между звуками: <b>{this.state.countLetters}</b></p>
-                            <input className="footer-edit__count-letters" type="range" name="" max="100" min="0" onChange={this.f_countLetters} value={this.state.countLetters} />
+                            <h5 className='settings-menu_pole__p'>Время между звуками: <b>{this.state.countLetters}</b></h5>
+                            <div className='settings-menu_pole__out'>
+                                <input className="footer-edit__count-letters" type="range" name="" max="100" min="0" onChange={this.f_countLetters} value={this.state.countLetters} />
+                                <p className='settings-menu_pole__out_n2'>55</p>
+                            </div>
                         </li>
                     </ul>
                 </div>
